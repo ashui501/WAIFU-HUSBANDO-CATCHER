41069a66b661335445ba1fd4b015b5d3d438d728
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-01-31 20:13:51 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "1784606556"
-    sudo_users = "1784606556"
-    GROUP_ID = -1001862219857
-    TOKEN = "5970924128:AAExV-nLSyXssUyD_8M4eSBgYppdME4PfMw"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "6649432492"
+    sudo_users = "6649432492"
+    GROUP_ID = -100212448722
+    TOKEN = "6713160483:AAGgrbbljK05FwaOxwJ0pTs4o1kcZNL0024"
+    mongo_url = "mongodb+srv://memek:memek@cluster0.xktxnyr.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "csbsupport"
-    UPDATE_CHAT = "ciliksmall"
+    SUPPORT_CHAT = "tgcanimecommunity"
+    UPDATE_CHAT = "Origanimeinfo"
     BOT_USERNAME = "ciliksmall9bot"
-    CHARA_CHANNEL_ID = "-1001862219857"
+    CHARA_CHANNEL_ID = "-1002124487227"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
