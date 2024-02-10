819299288e214d9a65be31037374aa03e832f734
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-11 00:52:18 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -10,7 +10,7 @@ class Config(object):
     PHOTO_URL = ["https://telegra.ph/file/43345aa900233f7649309.jpg", "https://telegra.ph/file/ee98b297887d361544b3c.jpg"]
     SUPPORT_CHAT = "gcanimecommunity"
     UPDATE_CHAT = "Origanimeinfo"
-    BOT_USERNAME = "AnimeharemindoBot"
+    BOT_USERNAME = "guess_hharem_Bot"
     CHARA_CHANNEL_ID = "-1002124487227"
     api_id = 17204044
     api_hash = "db2dd5d8401971aa433ef3c0f9a108da"
