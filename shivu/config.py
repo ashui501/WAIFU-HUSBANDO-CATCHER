f5357d70e39f8c1b2eaca2728b0a7141652eb654
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-10 16:59:17 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,17 +3,17 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6649432492"
-    sudo_users = "6649432492"
+    sudo_users = "6649432492", "5843270062"
     GROUP_ID = -100212448722
-    TOKEN = "6713160483:AAGgrbbljK05FwaOxwJ0pTs4o1kcZNL0024"
+    TOKEN = "6325135049:AAFSGBfpuBkMbzN2lTxwa8DxHZzwi8wurKk"
     mongo_url = "mongodb+srv://memek:memek@cluster0.xktxnyr.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    PHOTO_URL = ["https://telegra.ph/file/43345aa900233f7649309.jpg", "https://telegra.ph/file/ee98b297887d361544b3c.jpg"]
     SUPPORT_CHAT = "gcanimecommunity"
     UPDATE_CHAT = "Origanimeinfo"
-    BOT_USERNAME = "AnimeharemindoBot"
+    BOT_USERNAME = "Guess_harem_bot"
     CHARA_CHANNEL_ID = "-1002124487227"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = 17204044
+    api_hash = "db2dd5d8401971aa433ef3c0f9a108da"
 
     
 class Production(Config):
