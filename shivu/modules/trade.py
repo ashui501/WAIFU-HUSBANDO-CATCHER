fca0dd18bad1f3d4694eb5e1a5a9f836f0d83e0d
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-17 02:05:26 +0700

    Update trade.py

--- shivu/modules/trade.py
+++ shivu/modules/trade.py
@@ -57,8 +57,8 @@ async def trade(client, message):
     
     keyboard = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("Confirm Trade", callback_data="confirm_trade")],
-            [InlineKeyboardButton("Cancel Trade", callback_data="cancel_trade")]
+            [InlineKeyboardButton("✅ Confirm Trade", callback_data="confirm_trade")],
+            [InlineKeyboardButton("❌ Cancel Trade", callback_data="cancel_trade")]
         ]
     )
 
@@ -159,8 +159,8 @@ async def gift(client, message):
     
     keyboard = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("Confirm Gift", callback_data="confirm_gift")],
-            [InlineKeyboardButton("Cancel Gift", callback_data="cancel_gift")]
+            [InlineKeyboardButton("✅ Confirm Gift", callback_data="confirm_gift")],
+            [InlineKeyboardButton("✅ Cancel Gift", callback_data="cancel_gift")]
         ]
     )
 
