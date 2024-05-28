51453934c7e72a8b72975e328e5c9744734d417f
Ashui <113001130+ashui501@users.noreply.github.com>
2024-05-28 06:58:01 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6534367642"
     sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002049314623
+    GROUP_ID = -1002004197830
     TOKEN = "7063220368:AAEty7C6Z0MrlmUpmPH81J-AhM7Jyum4oOI"
     mongo_url = "mongodb+srv://tiwarireeta004:peqxLEd36RAg7ors@cluster0.furypd3.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://graph.org/file/cb65e2e8ea675f4b494af.jpg", "https://graph.org/file/1e64d77e0bec543c39f45.jpg"]
     SUPPORT_CHAT = "NAMIKAZECPAN"
     UPDATE_CHAT = "NAMIKAZECPAN"
     BOT_USERNAME = "GUESSEM_ALL_ROBOT"
-    CHARA_CHANNEL_ID = "-1002049314623"
+    CHARA_CHANNEL_ID = "-1002004197830"
     api_id = 24074986
     api_hash = "f4f6272a85d0e50e39a24cb378be118d"
 
