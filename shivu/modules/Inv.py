fbe582a7734cf42825b0a313f32322606f0793cf
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-06-22 11:20:12 +0530

    Create Inv.py

---
+++ shivu/modules/Inv.py
@@ -0,0 +1,18 @@
+from shivu import app
+from pyrogram import filters
+from Database.status_db import (
+    get_user_data,
+    get_user_Core,
+)
+
+@app.on_message(filters.command("inv"))
+async def inv(app, message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    current = await get_user_data(user_id)
+    status_text = "___\n"
+    status_text += f"🥋Name: {user_name}\n"
+    status_text += f"🪪Id: {user_id}\n"
+    status_text += "___\n"
+    status_text += f"💠 coins: {current['Core']}\n"
+    await message.reply(status_text)
